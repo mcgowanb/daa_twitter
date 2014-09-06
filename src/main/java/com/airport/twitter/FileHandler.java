7527e55ade371e8dae3cd8adf91c1db898a0a69a
Brian McGowan <mcgowan.b@gmail.com>
2014-09-06 13:47:29 +0100

    Tidied up after completed Junit testing

--- src/main/java/com/airport/twitter/FileHandler.java
+++ src/main/java/com/airport/twitter/FileHandler.java
@@ -6,53 +6,50 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.List;
 
 public class FileHandler {
-	private ArrayList<String> newArrivalsList;
-	private ArrayList<String> newDeparturesList;
-	private ArrayList<String> historicalArrivalsList;
-	private ArrayList<String> historicalDeparturesList;
-	private ArrayList<String> arrivalsToSaveExternally;
-	private ArrayList<String> departuresToSaveExternally;
-	private String arrLocation, depLocation;
+	private List<String> newArrivalsList = new ArrayList<String>();
+	private List<String> newDeparturesList = new ArrayList<String>();
+	private List<String> historicalArrivalsList = new ArrayList<String>();
+	private List<String> historicalDeparturesList = new ArrayList<String>();
+	private List<String> arrivalsToSaveExternally = new ArrayList<String>();
+	private List<String> departuresToSaveExternally = new ArrayList<String>();
+	private static String arrLocation = "src/main/resources/arrivals.txt";
+	private static String depLocation = "src/main/resources/departures.txt";
 
 	public FileHandler() {
-		this.arrLocation = "src/main/resources/arrivals.txt";
-		this.depLocation = "src/main/resources/departures.txt";
 	}
 
-	
-	public FileHandler(ArrayList<String> arrivals, ArrayList<String> departures) {
-		this();
+	public FileHandler(List<String> arrivals, List<String> departures) {
 		this.newArrivalsList = this.arrivalsToSaveExternally = arrivals;
 		this.newDeparturesList = this.departuresToSaveExternally = departures;
 	}
-	
-	public void checkForNewData(){
-		if (newArrivalsList.isEmpty() & newDeparturesList.isEmpty()){
+
+	public void checkForNewData() {
+		if (newArrivalsList.isEmpty() & newDeparturesList.isEmpty()) {
 			System.out.println("There are no flights to analyse");
 			System.exit(0);
 		}
 	}
-	
-	public void executeFileActions() throws IOException{
+
+	public void executeFileActions() throws IOException {
 		historicalArrivalsList = loadSavedFlightsFromFile(arrLocation);
 		newArrivalsList = removePreviouslyTweetedFlights(newArrivalsList, historicalArrivalsList);
 		historicalDeparturesList = loadSavedFlightsFromFile(depLocation);
 		newDeparturesList = removePreviouslyTweetedFlights(newDeparturesList, historicalDeparturesList);
-		checkForNewData();	//So, need to only remove the flights that have matches from the file, otherwise there will be multiple postings of stuff
+		checkForNewData(); // So, need to only remove the flights that have
+							// matches from the file, otherwise there will be
+							// multiple postings of stuff
 		clearFile(arrLocation);
 		clearFile(depLocation);
 		saveFlightsToFile(arrLocation, arrivalsToSaveExternally);
 		saveFlightsToFile(depLocation, departuresToSaveExternally);
 	}
-	
 
-	public ArrayList<String> loadSavedFlightsFromFile(String location) throws IOException {
+	public List<String> loadSavedFlightsFromFile(String location) throws IOException {
 		String str = null;
-		ArrayList<String> returnList = new ArrayList<String>();
+		List<String> returnList = new ArrayList<String>();
 		BufferedReader br = new BufferedReader(new FileReader(location));
 		while ((str = br.readLine()) != null) {
 			returnList.add(str);
@@ -62,30 +59,20 @@ public class FileHandler {
 		return returnList;
 
 	}
-	
-	public void saveFlightsToFile(String location, ArrayList<String> list) throws IOException{
+
+	public void saveFlightsToFile(String location, List<String> list) throws IOException {
 		BufferedWriter bw = new BufferedWriter(new FileWriter(location));
-		for(String s : list){
-			bw.write(s);	
+		for (String s : list) {
+			bw.write(s);
 			bw.newLine();
 		}
 		bw.close();
-		
+
 	}
 
+	public List<String> removePreviouslyTweetedFlights(List<String> newList, List<String> oldList) {
 
-	public ArrayList<String> removePreviouslyTweetedFlights(ArrayList<String> newList, ArrayList<String> oldList) {
-
-		for (Iterator<String> iterator = oldList.iterator(); iterator.hasNext();) {
-			String string = iterator.next();
-			for (Iterator<String> iter = newList.iterator(); iter.hasNext();) {
-				String str = iter.next();
-				if (str.equals(string)) {
-					iter.remove();
-					continue;
-				}
-			}
-		}
+		newList.removeAll(oldList);
 		return newList;
 	}
 
@@ -94,33 +81,20 @@ public class FileHandler {
 		bw.close();
 	}
 
-	public ArrayList<String> parseMapToList(Map<String, String> oldList) {
-		ArrayList<String> newList = new ArrayList<String>();
-
-		for (Map.Entry<String, String> entry : oldList.entrySet()) {
-			newList.add(entry.getValue());
-		}
-		return newList;
-	}
-
-
-	public Object getArrLocation() {
+	public String getArrLocation() {
 		return arrLocation;
 	}
 
-
-	public Object getDepLocation() {
+	public String getDepLocation() {
 		return depLocation;
 	}
 
-
-	public Object getNewArrivalsList() {
+	public List<String> getNewArrivalsList() {
 		// TODO Auto-generated method stub
 		return newArrivalsList;
 	}
 
-
-	public Object getNewDeparturesList() {
+	public List<String> getNewDeparturesList() {
 		// TODO Auto-generated method stub
 		return newDeparturesList;
 	}
